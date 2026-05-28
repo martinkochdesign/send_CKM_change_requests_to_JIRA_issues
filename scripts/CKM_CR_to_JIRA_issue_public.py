2a435f9b7b11a5061abc161f573aa703a4f3cfc2
martinkochdesign <72647122+martinkochdesign@users.noreply.github.com>
2026-05-28 09:46:17 +0200

    Update CKM_CR_to_JIRA_issue_public.py

--- scripts/CKM_CR_to_JIRA_issue_public.py
+++ scripts/CKM_CR_to_JIRA_issue_public.py
@@ -241,8 +241,8 @@ print('Number of issues', len(issue_info))
 print('Checking for all CKM CRs...')
 # for every CKM Chang Request...
 #for CR in changeRequest:
-#for i in range(len(changeRequest)):
-for i in range(200):   #MAKE IT SO WE CAN ONLY SEND N CR TO JIRA
+for i in range(len(changeRequest)):
+#for i in range(200):   #MAKE IT SO WE CAN ONLY SEND N CR TO JIRA
 	CR = changeRequest[i]
 	print('Looking at CR', CR['crId'])
 
