e2a3f9abd245dc4e38e63860696f078a023b413d
martinkochdesign <72647122+martinkochdesign@users.noreply.github.com>
2026-05-28 08:24:47 +0200

    Update CKM_CR_to_JIRA_issue_public.py
    Prepared to handle 50 CRs

--- scripts/CKM_CR_to_JIRA_issue_public.py
+++ scripts/CKM_CR_to_JIRA_issue_public.py
@@ -10,7 +10,7 @@ from pathlib import Path
 
 version = "0.5.0"
 LOCAL = False
-READWRITE = False
+READWRITE = True
 
 # Jira instance info
 JIRA_URL = "https://openehr.atlassian.net"
@@ -241,8 +241,8 @@ print('Number of issues', len(issue_info))
 print('Checking for all CKM CRs...')
 # for every CKM Chang Request...
 #for CR in changeRequest:
-for i in range(len(changeRequest)):
-#for i in range(500):   #MAKE IT SO WE CAN ONLY SEND ONE CR TO JIRA
+#for i in range(len(changeRequest)):
+for i in range(50):   #MAKE IT SO WE CAN ONLY SEND N CR TO JIRA
 	CR = changeRequest[i]
 	print('Looking at CR', CR['crId'])
 
